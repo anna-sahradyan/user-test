6afa367d4f3f6ef026b824faeae9f5d9e575c7ee
anna-sahradyan <a.sahradyan72@gmail.com>
2023-06-22 20:44:30 +0400

    changed index.js file

--- server/index.js
+++ server/index.js
@@ -6,7 +6,17 @@ dotenv.config();
 const cors = require("cors");
 const connectDB = require("./config/connectDB");
 const userRouter = require('./routes/user-router.js');
-app.use(cors({origin:true}));
+
+app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Credentials", "true");
+    res.set("Access-Control-Max-Age", "1800");
+    res.set("Access-Control-Allow-Headers", "content-type");
+    next();
+});
+app.use(cors({origin: "http://localhost:3000",
+    credentials: true,
+    exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],}));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 //?DEPLOYMENT CODE
@@ -15,8 +25,8 @@ app.get("/", (req, res) => {
 });
 //?Router
 app.use("/api/users", userRouter);
-
 connectDB();
+
 app.listen(PORT, () => {
     console.log(` Server is running in http://localhost:${PORT}`)
 })
